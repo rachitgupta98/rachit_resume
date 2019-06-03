d65659631bea116c64b04aaf70788486c3b48146
Rachit <rachit371@gmail.com>
2019-06-03 14:19:35 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Particles from "react-particles-js";
 const particlesOptions = {
   particles: {
     number: {
-      value: 100,
+      value: 80,
       density: {
         enable: true,
         value_area: 800
@@ -30,8 +30,89 @@ const particlesOptions = {
       polygon: {
         nb_sides: 5
       }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 2,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 60,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 2
+    },
+    move: {
+      enable: true,
+      speed: 8,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
     }
-  }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: false,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 100,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 100,
+        size: 80,
+        duration: 2,
+        opacity: 0.8,
+        speed: 3
+      },
+      repulse: {
+        distance: 400,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
 };
 class AppStart extends Component {
   render() {
@@ -44,7 +125,6 @@ class AppStart extends Component {
         <Education />
         <Skill />
         <Achievements />
-
         <Particles className="particles" params={particlesOptions} />
       </Fragment>
     );
